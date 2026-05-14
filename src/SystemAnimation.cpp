e85628d925ac555d744371afb06e201b5deaaae2
Allan Majid <allan.majid02@gmail.com>
2026-05-14 13:43:43 +0200

    Refactored animationsystem by extracting into helper functions

--- src/SystemAnimation.cpp
+++ src/SystemAnimation.cpp
@@ -3,10 +3,8 @@
 #include "ComponentMesh.hpp"
 #include "RenderableMesh.hpp"
 #include "ComponentLinearVelocity.hpp"
-#include "ComponentNPCController.hpp"
 #include <glm/geometric.hpp>
 #include <algorithm>
-
 
 void AnimationSystem::Update(entt::registry& registry, float deltaTime)
 {
@@ -15,7 +13,6 @@ void AnimationSystem::Update(entt::registry& registry, float deltaTime)
 	for (auto entity : view)
 	{
 		auto& animationComponent = view.get<AnimationComponent>(entity);
-
 		auto& meshComponent = view.get<MeshComponent>(entity);
 
 		auto mesh = meshComponent.mesh.lock();
@@ -29,39 +26,54 @@ void AnimationSystem::Update(entt::registry& registry, float deltaTime)
 
 		if (animationComponent.useLayering)
 		{
-			if (animationComponent.upperBodyRootNode.empty())
-			{
-				mesh->animateBlend(animationComponent.baseAnimation, animationComponent.secondaryAnimation, animationComponent.time, animationComponent.time, animationComponent.blendFactor);
-				continue;
-			}
-
-			eeng::AnimationBranchDesc upperBodyFilter;
-			upperBodyFilter.root_node_name = animationComponent.upperBodyRootNode;
-
-			mesh->animateBlend(animationComponent.baseAnimation, animationComponent.secondaryAnimation, animationComponent.time, animationComponent.time, upperBodyFilter);
+			ApplyLayeredAnimation(mesh, animationComponent);
 		}
 		else
 		{
 			if (animationComponent.useSpeedControl)
 			{
-				float movementSpeed = 0.0f;
-
-				if (auto* velocityComponent = registry.try_get<LinearVelocityComponent>(entity))
-				{
-					movementSpeed = glm::length(velocityComponent->velocity);
-				}
-
-				float animationWalkSpeed = 4.0f;
-				float targetSpeed = std::clamp(movementSpeed / animationWalkSpeed, 0.0f, 1.0f);
-
-				animationComponent.speed = glm::mix(animationComponent.speed, targetSpeed, 0.1f);
-
-				animationComponent.baseAnimation = 1;      
-				animationComponent.secondaryAnimation = 2; 
-				animationComponent.blendFactor = animationComponent.speed;
+				UpdateSpeedControl(registry, entity, animationComponent);
 			}
 
-			mesh->animateBlend(animationComponent.baseAnimation, animationComponent.secondaryAnimation, animationComponent.time, animationComponent.time, animationComponent.blendFactor);
+			ApplyFullBodyBlend(mesh, animationComponent);
 		}
 	}
+}
+
+void AnimationSystem::UpdateSpeedControl(entt::registry& registry, entt::entity entity, AnimationComponent& animationComponent)
+{
+	float movementSpeed = 0.0f;
+
+	if (auto* velocityComponent = registry.try_get<LinearVelocityComponent>(entity))
+	{
+		movementSpeed = glm::length(velocityComponent->velocity);
+	}
+
+	float animationWalkSpeed = 4.0f;
+	float targetSpeed = std::clamp(movementSpeed / animationWalkSpeed, 0.0f, 1.0f);
+
+	animationComponent.speed = glm::mix(animationComponent.speed, targetSpeed, 0.1f);
+
+	animationComponent.baseAnimation = 1;
+	animationComponent.secondaryAnimation = 2;
+	animationComponent.blendFactor = animationComponent.speed;
+}
+
+void AnimationSystem::ApplyLayeredAnimation(std::shared_ptr<eeng::RenderableMesh> mesh, AnimationComponent& animationComponent)
+{
+	if (animationComponent.upperBodyRootNode.empty())
+	{
+		ApplyFullBodyBlend(mesh, animationComponent);
+		return;
+	}
+
+	eeng::AnimationBranchDesc upperBodyFilter;
+	upperBodyFilter.root_node_name = animationComponent.upperBodyRootNode;
+
+	mesh->animateBlend(animationComponent.baseAnimation, animationComponent.secondaryAnimation, animationComponent.time, animationComponent.time, upperBodyFilter);
+}
+
+void AnimationSystem::ApplyFullBodyBlend(std::shared_ptr<eeng::RenderableMesh> mesh, AnimationComponent& animationComponent)
+{
+	mesh->animateBlend(animationComponent.baseAnimation, animationComponent.secondaryAnimation, animationComponent.time, animationComponent.time, animationComponent.blendFactor);
 }
--- src/SystemAnimation.hpp
+++ src/SystemAnimation.hpp
@@ -1,8 +1,21 @@
 #pragma once
 #include <entt/entt.hpp>
+#include <memory>
+
+namespace eeng
+{
+	class RenderableMesh;
+}
+
+struct AnimationComponent;
 
 class AnimationSystem
 {
-	public:
-		void Update(entt::registry& registry, float deltaTime);
+public:
+	void Update(entt::registry& registry, float deltaTime);
+
+private:
+	void UpdateSpeedControl(entt::registry& registry, entt::entity entity, AnimationComponent& animationComponent);
+	void ApplyLayeredAnimation(std::shared_ptr<eeng::RenderableMesh> mesh, AnimationComponent& animationComponent);
+	void ApplyFullBodyBlend(std::shared_ptr<eeng::RenderableMesh> mesh, AnimationComponent& animationComponent);
 };
